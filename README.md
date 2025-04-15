2c68075d00267e08a147102a7a3116b6d0e3ffec
QWERTYphish <julien.mansier@gmail.com>
2025-04-15 18:38:19 +0000

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # analyzeANDwait
 Send a file to Spectra Analyze, then wait for the analysis to be done.
+
+## Requirements
+1) Python Requests installed via PyPi: https://pypi.org/project/requests/
+
+#### Command Line Arguments
+1) --filePath : 
+2) --sampleName : Name you want to appear in the Analyze Dashboard
+3) --token : API token
+4) --url : URL of the A1000/Analyze instance
+5) --wait : Waits until the file is finished processing before ending the script 
+
+##### Example
+```
+python3 send2analyze.py --filePath ./Examples/7zz --sampleName 7z --token 80sr098d098sdf098gsdf --url a1000.example.com --wait true
+```
---
+++ send2analyze.py
@@ -0,0 +1,74 @@
+import requests
+import argparse
+import json
+import time
+
+wait = False
+token = ""
+url = ""
+
+def ready(hashid):
+    full_url = "https://"+url+"/api/samples/status/"
+    payload = {"hash_values": [hashid]}
+
+    headers = {
+        "Authorization": f"Token {token}"
+    }
+
+    # Add verify=False in the request if you are using a self-signed SSL certificate
+    response = requests.post(full_url, headers=headers, json=payload)
+
+    if response.ok:
+        status = response.json()["results"][0]["status"]
+        return status
+    else:
+        # Issue with request
+        print(f"Status Code: {response.status_code}")
+        print(response.text)
+        return "ERROR"
+
+def main():
+    global wait, token, url
+
+    parser = argparse.ArgumentParser(description="Process needed files")
+    parser.add_argument('--filePath', type=str,required=True, help="Full File Path")
+    parser.add_argument('--sampleName', type=str,required=True, help="File Name")
+    parser.add_argument( '--token', type=str,required=True, help="API Token")
+    parser.add_argument( '--url', type=str,required=True, help="Spectra Analyze URL")
+    parser.add_argument( '--wait', type=str, help="Wait until processing completes?")
+    args = parser.parse_args()
+
+    # Change the values of hash_value and token
+    file_path = args.filePath
+    sample_name = args.sampleName
+    token = args.token
+    url = args.url
+
+    if 'true' in args.wait.lower():
+        wait = True
+
+    full_url = "https://"+url+"/api/uploads/"
+
+    files=[('file', (sample_name, open(file_path,'rb'),'application/octet-stream'))]
+
+    headers = {"Authorization": f"Token {token}"}
+    
+    response = requests.post(full_url, headers=headers, files=files)
+
+    if response.ok:
+        hashid = response.json()["detail"]["sha1"]
+    else:
+        # Issue with request
+        print(f"Status Code: {response.status_code}")
+
+    
+    if wait:
+        while ready(hashid) != "processed":
+            print("Waiting on sample to process...")
+            time.sleep(1)
+        print("Sample is processed!")
+
+
+
+if __name__ == "__main__":
+    main()
